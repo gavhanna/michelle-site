9685a610e0ab309c2b4263dee8b144041bb4c0a5
Gavin Hanna <gavhanna@gmail.com>
2016-10-22 18:27:18 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("smaller");
+    } else {
+       $(".header").removeClass("smaller");
+    }
+});
